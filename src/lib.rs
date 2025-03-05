9d4aa7ac3f799ed5f035452551ca719a69219433
Violet <larsona@outlook.com>
2025-03-05 09:50:39 -0600

    new rewrite!

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 #![allow(unused, dead_code)]
 
-// pub mod bisection;
+use std::collections::HashMap;
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Bisection<I: Clone, S> {
+    /// I is the index type, something short and clonable
+    objects: Vec<I>,
+    /// state that handles the various object types
+    /// if your object type isnt clone you might keep it here and use
+    /// numeric indices
+    state: S,
+}
--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,6 @@ pub fn main(
     eprintln!(
         "Warning! Halfwit is unfinished software. You should backup all relevant files yourself."
     );
-    // TODO: way to restore a manifest automatically on previous failure
-    let _ = fs::remove_dir_all(".halfwit"); // rm -f
-    fs::create_dir(".halfwit")?;
-    let mut manifest: HashMap<Uuid, &Path> = HashMap::new();
     let mut paths: Vec<PathBuf> = Vec::new();
     for path in files {
         if let Ok(paths_to_add) = glob(&path) {
@@ -44,91 +40,4 @@ pub fn main(
         }
     }
     println!("paths: {:#?}", &paths);
-
-    // copy the files to the manifest
-    for path in paths.iter() {
-        let file_uuid = Uuid::new_v4();
-        manifest.insert(file_uuid, path);
-        fs::copy(path, format!(".halfwit/{}", file_uuid))?;
-    }
-    let mut mfile = File::create(".halfwit/MANIFEST")?;
-    write!(mfile, "{:#?}", manifest); // TODO: use serde here so we can deserialize this too?
-
-    #[cfg(windows)]
-    let shell = shell.unwrap_or("powershell.exe".to_owned());
-    #[cfg(unix)]
-    let shell = shell.unwrap_or("sh".to_owned());
-    #[cfg(not(any(windows, unix)))]
-    compile_error!(
-        "Compiling on something that's neither windows nor unix. \
-        Please raise an issue and tell me what you're trying to do here. \
-        https://github.com/typecasto/halfwit"
-    );
-    let mut run = Command::new(shell);
-    run.arg("-c").arg(&command);
-    run.stdin(Stdio::null());
-    run.stdout(Stdio::null());
-    run.stderr(Stdio::null());
-
-    // Verify that script works as intended
-    // TODO: determine if this is useful
-    // assert_eq!(run.status()?.success(), false); // TODO: better errors
-    // TODO: genericize
-    for path in manifest.values() {
-        fs::remove_file(path)?;
-    }
-    // assert_eq!(run.status()?.success(), true); // TODO: better errors
-    restore_manifest(&manifest);
-
-    // Start the bisection!
-    let mut stack: Vec<Vec<Uuid>> = vec![manifest.keys().map(|x| x.to_owned()).collect()];
-    let mut bad: Vec<Uuid> = Vec::new();
-    while !stack.is_empty() {
-        let next = stack.pop().unwrap();
-        // todo: genericize
-        for (uuid, path) in manifest.iter() {
-            if next.contains(uuid) {
-                if !path.exists() {
-                    fs::copy(format!(".halfwit/{}", uuid), path)?;
-                }
-            } else {
-                if path.exists() {
-                    fs::remove_file(path)?;
-                }
-            }
-        }
-        println!("Testing batch of {} files.", next.len());
-        // run and handle the results
-        match run.status()?.success() {
-            true => {
-                // do nothing?
-            }
-            false if next.len() == 1 => {
-                println!(
-                    "bad element found: {}",
-                    manifest.get(&next[0]).unwrap().to_string_lossy()
-                );
-                bad.push(next[0]);
-            }
-            false => {
-                let (a, b) = next.split_at(next.len() / 2);
-                stack.push(a.to_owned());
-                stack.push(b.to_owned());
-            }
-        }
-    }
-    println!("Bad files were:");
-    for uuid in bad.iter() {
-        println!("{}", manifest.get(uuid).unwrap().to_string_lossy());
-    }
-    restore_manifest(&manifest);
-
-    Ok(())
-}
-
-fn restore_manifest(manifest: &HashMap<Uuid, &Path>) -> R<()> {
-    for (uuid, path) in manifest {
-        fs::copy(format!(".halfwit/{}", uuid), path)?;
-    }
-    Ok(())
 }
